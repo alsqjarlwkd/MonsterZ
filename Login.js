e3459e4866379b54e520c5f1e90d4f5eb92b4dcd
MinBeomgi <alsqjarlwkd@naver.com>
2021-03-29 18:28:02 +0900

    알림창 삭제
    알림창 삭제

--- Login.js
+++ Login.js
@@ -1,10 +1,8 @@
 const LoginBtn = document.querySelector(`.LoginBtn`);
 const JoinUsBtn = document.querySelector(`.JoinUsBtn`);
 function ClickLogin(){
-    alert("현재 해당 기능 구현중입니다. 잠시만 기다려주세요!")
 }
 function ClickJoinUs(){
-    alert("현재 해당 기능 구현중입니다. 잠시만 기다려주세요!")
 }
 function init(){
     LoginBtn.addEventListener("click",ClickLogin);
