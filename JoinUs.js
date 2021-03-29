92b1cd25c5c31b7e7cfdf1e3717c736fd92d5829
MinBeomgi <alsqjarlwkd@naver.com>
2021-03-29 15:19:27 +0900

    회원가입 텍스트 없을때 경고 문구 출력
    회원가입 텍스트 없을때 경고 문구 출력

--- JoinUs.js
+++ JoinUs.js
@@ -1,17 +1,59 @@
-const inputID = document.querySelectorAll(`.inputId`);
-const MustBeTyping = document.querySelectorAll(".MustTypingId");
-let inputValue = document.querySelectorAll(".inputId").value;
+const InputId = document.getElementsByClassName(`inputId`);
+const MustBeTyping = document.getElementsByClassName(`MustTypingId`);
 
-
-function Checkinput(e){
-
-
+function Checkinput(){
+    if(InputId[0].value === "")
+    {
+        MustBeTyping[0].style.display=`block`;
+    }
+    else{
+        MustBeTyping[0].style.display=`none`;
+    }
 }
+
+function Checkinput2(){
+    if(InputId[1].value === "")
+    {
+        MustBeTyping[1].style.display=`block`;
+    }
+    else{
+        MustBeTyping[1].style.display=`none`;
+    }
+}
+
+function Checkinput3(){
+    if(InputId[2].value === "")
+    {
+        MustBeTyping[2].style.display=`block`;
+    }
+    else{
+        MustBeTyping[2].style.display=`none`;
+    }
+}
+function Checkinput4(){
+    if(InputId[3].value === "")
+    {
+        MustBeTyping[3].style.display=`block`;
+    }
+    else{
+        MustBeTyping[3].style.display=`none`;
+    }
+}
+function Checkinput5(){
+    if(InputId[4].value === "")
+    {
+        MustBeTyping[4].style.display=`block`;
+    }
+    else{
+        MustBeTyping[4].style.display=`none`;
+    }
+}
+
 function init(){
-    for(let i = 0;i<inputID.length;i++)
-{
-    inputID[i].addEventListener("click",Checkinput);
+    InputId[0].addEventListener("click",Checkinput);
+    InputId[1].addEventListener("click",Checkinput2);
+    InputId[2].addEventListener("click",Checkinput3);
+    InputId[3].addEventListener("click",Checkinput4);
+    InputId[4].addEventListener("click",Checkinput5);
 }
-}
-
 init();
