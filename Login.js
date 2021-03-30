72d1a5086a462fc3892d503183ae9b0f89c5a84d
BENI <ajc9494@gmail.com>
2021-03-30 10:32:19 +0900

    로그인 버튼 클릭
    로그인 클릭 시 공백 체크 및 전송

--- Login.js
+++ Login.js
@@ -1,6 +1,19 @@
 const LoginBtn = document.querySelector(`.LoginBtn`);
 const JoinUsBtn = document.querySelector(`.JoinUsBtn`);
 function ClickLogin(){
+    var Form = document.querySelector('.LoginForm');
+    var ID = document.getElementById('ID');
+    var PWD = document.getElementById('PWD');
+
+    if(ID.value === ""){
+        alert("아이디를 입력해주세요.");
+    }
+    else if(PWD.value === ""){
+        alert("비밀번호를 입력해주세요.");
+    }
+    else if(ID.value != "" & PWD.value != ""){
+        Form.submit();
+    }
 }
 function ClickJoinUs(){
 }
