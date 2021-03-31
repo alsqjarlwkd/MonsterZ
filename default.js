f9047624efb802de56775b1276142443b068bbae
MinBeomgi <alsqjarlwkd@naver.com>
2021-03-31 13:40:43 +0900

    Main 페이지 Navbar 부분 작성(JQuery 이용)
    Main 페이지 Navbar 부분 작성(JQuery 이용)

---
+++ default.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#menu1").mouseover(function(){
+        $("#menu_sub1").slideDown(1000)
+    });
+})
+$(document).ready(function(){
+    $("#menu2").mouseover(function(){
+        $("#menu_sub2").slideDown(1000)
+    });
+})
+
+$(document).ready(function(){
+    $("#menu3").mouseover(function(){
+        $("#menu_sub3").slideDown(1000)
+    });
+})
+
+$(document).ready(function(){
+    $("#menu4").mouseover(function(){
+        $("#menu_sub4").slideDown(1000)
+    });
+})
+
+$(document).ready(function(){
+    $("#menu5").mouseover(function(){
+        $("#menu_sub5").slideDown(1000)
+    });
+})
