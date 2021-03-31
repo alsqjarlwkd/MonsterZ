30ace4a91d314d74145c89ff9922e1ffb124099b
MinBeomgi <alsqjarlwkd@naver.com>
2021-03-31 14:00:20 +0900

    선택자 id에서 class로 변경
    선택자 id에서 class로 변경

--- default.js
+++ default.js
@@ -1,44 +1,36 @@
 $(document).ready(function(){
-    $("#menu1").mouseover(function(){
-        $("#menu_sub1").slideDown(200)
+    $(".menu1").mouseover(function(){
+        $(".menu_sub1").slideDown(200)
     });
-    $("#menu1").mouseout(function(){
-        $("#menu_sub1").slideUp(200)
+    $(".menu1").mouseout(function(){
+        $(".menu_sub1").slideUp(200)
+    });
+
+    $(".menu2").mouseover(function(){
+        $(".menu_sub2").slideDown(200)
+    });
+    $(".menu2").mouseout(function(){
+        $(".menu_sub2").slideUp(200)
+    });
+
+    $(".menu3").mouseover(function(){
+        $(".menu_sub3").slideDown(200)
+    });
+    $(".menu3").mouseout(function(){
+        $(".menu_sub3").slideUp(200)
     });
     
-})
-$(document).ready(function(){
-    $("#menu2").mouseover(function(){
-        $("#menu_sub2").slideDown(200)
+    $(".menu4").mouseover(function(){
+        $(".menu_sub4").slideDown(200)
     });
-    $("#menu2").mouseout(function(){
-        $("#menu_sub2").slideUp(200)
+    $(".menu4").mouseout(function(){
+        $(".menu_sub4").slideUp(200)
     });
-})
 
-$(document).ready(function(){
-    $("#menu3").mouseover(function(){
-        $("#menu_sub3").slideDown(200)
+    $(".menu5").mouseover(function(){
+        $(".menu_sub5").slideDown(200)
     });
-    $("#menu3").mouseout(function(){
-        $("#menu_sub3").slideUp(200)
-    });
-})
-
-$(document).ready(function(){
-    $("#menu4").mouseover(function(){
-        $("#menu_sub4").slideDown(200)
-    });
-    $("#menu4").mouseout(function(){
-        $("#menu_sub4").slideUp(200)
-    });
-})
-
-$(document).ready(function(){
-    $("#menu5").mouseover(function(){
-        $("#menu_sub5").slideDown(200)
-    });
-    $("#menu5").mouseout(function(){
-        $("#menu_sub5").slideUp(200)
+    $(".menu5").mouseout(function(){
+        $(".menu_sub5").slideUp(200)
     });
 })
