f4920ad17a9bfad99f56cfac7cc1f138e6bc2ef1
BENI <ajc9494@gmail.com>
2021-04-03 03:43:19 +0900

    카카오 로그인 및 로그아웃
    카카오 로그인 및 로그아웃

---
+++ common.js
@@ -0,0 +1,24 @@
+const JAVASCRIPT_SDK_APP_KEY = '55a8e3f4af7b3fa4a8baa725b75248d2';
+
+window.addEventListener('load', init);
+
+function init() {
+    console.log('init');
+    Kakao.init(JAVASCRIPT_SDK_APP_KEY);
+    console.log(Kakao.isInitialized());
+
+    console.log(window.location.pathname)
+    if (isLoggedin()) {
+        if (window.location.pathname !== '/default.html') {
+            window.location.pathname = '/default.html';
+        }
+    } else {
+        if (window.location.pathname !== '/Login.html') {
+            window.location.pathname = '/Login.html';
+        }
+    }
+}
+
+function isLoggedin() {
+    return Boolean(Kakao.Auth.getAccessToken());
+}
---
+++ profile.js
@@ -0,0 +1,103 @@
+const accountInfo = {};
+
+window.addEventListener('load', initView);
+
+function initView() {
+    const btnKakaoLogout = document.getElementById('btn_kakao_logout');
+    btnKakaoLogout.addEventListener('click', logout);
+
+    requestData();
+}
+
+function logout() {
+    Kakao.Auth.logout(function() {
+        console.log(Kakao.Auth.getAccessToken());
+        window.location.href = ''
+    });
+}
+
+function requestData() {
+    Kakao.API.request({
+        url: '/v2/user/me',
+        success: function(response) {
+            initProfileView(response);
+            console.log(response);
+        },
+        fail: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function updateNickname(nickname) {
+    Kakao.API.request({
+        url: '/v1/user/update_profile',
+        data: {
+            properties: {
+                nickname
+            },
+        },
+        success: function(response) {
+            console.log(response);
+        },
+        fail: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function initProfileView(kakao_account) {
+    const { nickname, profile_image_url } = kakao_account.kakao_account.profile;
+    const { id } = kakao_account;
+
+    // document.getElementById('image_profile').setAttribute('src', profile_image_url);
+    document.getElementById('kakao_name').textContent = `${nickname}님 로그인 되었습니다.`;
+}
+// function sendSimpleMessage() {
+//   Kakao.Link.createCustomButton({
+//     templateId: 47342,
+//     container: '#btn_send_message',
+//     templateArgs: {
+//       title: '이게 설명?',
+//       description: '이건 고양이?',
+//       THU:
+//         'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+//     }
+//   });
+// }
+
+// function sendMessage() {
+//   Kakao.Link.createDefaultButton({
+//     container: '#btn_send_message',
+//     objectType: 'feed',
+//     content: {
+//       title: '디저트 사진',
+//       description: '아메리카노, 빵, 케익',
+//       imageUrl:
+//         'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+//       link: {
+//         mobileWebUrl: 'https://developers.kakao.com',
+//         androidExecParams: 'test',
+//       },
+//     },
+//     social: {
+//       likeCount: 10,
+//       commentCount: 20,
+//       sharedCount: 30,
+//     },
+//     buttons: [
+//       {
+//         title: '웹으로 이동',
+//         link: {
+//           mobileWebUrl: 'https://developers.kakao.com',
+//         },
+//       },
+//       {
+//         title: '앱으로 이동',
+//         link: {
+//           mobileWebUrl: 'https://developers.kakao.com',
+//         },
+//       },
+//     ]
+//   });
+// }
